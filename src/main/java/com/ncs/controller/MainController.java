bd69bae6500154d3811d088afbf44f2c97b13a34
wwoggml <kimjaehee18@hs.ac.kr>
2023-04-08 13:22:59 +0900

    카테고리 페이지 연결(2)

--- src/main/java/com/ncs/controller/MainController.java
+++ src/main/java/com/ncs/controller/MainController.java
@@ -1,14 +1,22 @@
 package com.ncs.controller;
 
 
+import com.ncs.entity.News;
+import com.ncs.service.NewsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
 
+    @Autowired
+    NewsService newsService;
     @GetMapping("/")
     public String main() {
         return "RNSEMain";
